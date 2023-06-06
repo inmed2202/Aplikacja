d721e810a34220f6f6fae1ec2cb3d55ebeaceaa2
Ina Miadzvedzeva <studia.ms.inmed.98@gmail.com>
2023-06-06 16:49:59 +0200

    video

--- app/src/main/java/com/example/loginapplication/TrenningsActivity.kt
+++ app/src/main/java/com/example/loginapplication/TrenningsActivity.kt
@@ -1,11 +1,23 @@
 package com.example.loginapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.loginapplication.databinding.ActivityCategoryBinding
+import com.example.loginapplication.databinding.ActivityTrenningsBinding
 
 class TrenningsActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityTrenningsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_trennings)
+        binding = ActivityTrenningsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.download1.setOnClickListener {
+            val intent = Intent(this, ObwodowyOnlineActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
